73ad72f1ff6cc4ab6b26830f37c358aa3361a6ae
anna.bonaldo <anna.bonaldo@bluewind.it>
2023-10-23 17:01:05 +0200

    blinky example

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("ciao");
+#![no_std]
+#![no_main]
+#[cfg_attr(target_arch = "tricore", panic_handler)]
+#[allow(unused)]
+fn panic(panic: &core::panic::PanicInfo<'_>) -> ! {
+
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
+
+tc37x_rt::entry!(main);
+
+fn main() -> ! {
+    loop{}
 }
