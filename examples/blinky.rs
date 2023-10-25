6e8d2d3cbe77b0e16bdef70b795aa799f7a7531b
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-10-25 12:59:45 +0200

    Auto format

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,9 +1,9 @@
 #![no_std]
 #![no_main]
+
 #[cfg_attr(target_arch = "tricore", panic_handler)]
 #[allow(unused)]
 fn panic(panic: &core::panic::PanicInfo<'_>) -> ! {
-
     #[allow(clippy::empty_loop)]
     loop {}
 }
@@ -11,5 +11,5 @@ fn panic(panic: &core::panic::PanicInfo<'_>) -> ! {
 tc37x_rt::entry!(main);
 
 fn main() -> ! {
-    loop{}
+    loop {}
 }
