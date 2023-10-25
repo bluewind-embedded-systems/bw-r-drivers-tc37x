522acf8f3a172d71277081c889c39e4132d3d0b1
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-10-25 12:58:55 +0200

    Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,1 @@
 #![no_std]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
