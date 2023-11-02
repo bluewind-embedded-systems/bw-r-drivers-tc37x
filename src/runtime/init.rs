14a5b18229e16374565527392c48ba79378798d8
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-11-02 15:33:40 +0100

    Disable watchdogs only if feature is enabled

--- src/runtime/init.rs
+++ src/runtime/init.rs
@@ -1,21 +1,26 @@
 use tc37x_rt::{
     asm_calls::read_cpu_core_id,
     isr::load_interrupt_table,
+    post_init, pre_init,
     wdtcon::{disable_cpu_watchdog, disable_safety_watchdog},
-    *,
 };
 
 pre_init!(pre_init_fn);
 #[allow(unused)]
 fn pre_init_fn() {
-    if read_cpu_core_id() == 0 {
-        disable_safety_watchdog();
-    }
-    disable_cpu_watchdog();
+    #[cfg(feature = "disable_watchdogs")]
+    disable_watchdogs();
 }
 
 post_init!(post_init_fn);
 #[allow(unused)]
 fn post_init_fn() {
     load_interrupt_table();
+}
+
+fn disable_watchdogs() {
+    if read_cpu_core_id() == 0 {
+        disable_safety_watchdog();
+    }
+    disable_cpu_watchdog();
 }
