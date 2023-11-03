cd9ce02c0cda948332b2dedc3fc92f63342492cd
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-11-03 16:22:19 +0100

    Add integration test

---
+++ tests/gpio.rs
@@ -0,0 +1,12 @@
+use tc37x_hal::tracing::*;
+
+#[test]
+fn test_pin_set_high() {
+    redirect_to_print();
+
+    use tc37x_hal::gpio::GpioExt;
+    use tc37x_pac::PORT_00;
+    let gpio00 = PORT_00.split();
+    let mut output = gpio00.p00_5.into_push_pull_output();
+    output.set_high();
+}
