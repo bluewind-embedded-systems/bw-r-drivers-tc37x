d67d80586b6296c968791c18ba47560fd55ec649
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-11-06 11:56:39 +0100

    Improve tracing

--- tests/gpio.rs
+++ tests/gpio.rs
@@ -5,18 +5,17 @@ use tc37x_pac::PORT_00;
 
 #[test]
 fn test_pin_set_high() {
-    test_with(
-        || LogEffectReporter::default(),
-        |r| {
-            let gpio00 = PORT_00.split();
-            let mut output = gpio00.p00_5.into_push_pull_output();
+    let reporter = LogEffectReporter::default();
 
-            output.set_high();
+    test_with(reporter, |r| {
+        let gpio00 = PORT_00.split();
+        let mut output = gpio00.p00_5.into_push_pull_output();
 
-            r.visit(|r| {
-                let logs = r.get_logs();
-                assert_eq!(logs.len(), 2);
-            });
-        },
-    );
+        output.set_high();
+
+        r.visit(|r| {
+            let logs = r.get_logs();
+            assert_eq!(logs.len(), 2);
+        });
+    });
 }
