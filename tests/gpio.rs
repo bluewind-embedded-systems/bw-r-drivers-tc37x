3a7b48ef9e25e0a5fa802816e30949922d5f39d3
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-11-09 11:22:50 +0100

    Add tests

--- tests/gpio.rs
+++ tests/gpio.rs
@@ -1,9 +1,10 @@
-use tc37x_hal::gpio::GpioExt;
-use tc37x_hal::tracing;
 use tc37x_pac::PORT_00;
 
+use tc37x_hal::gpio::GpioExt;
+use tc37x_hal::tracing;
+
 #[test]
-fn test_pin_set_high() {
+fn test_pin_set_high_and_low() {
     let report = tracing::log::Report::new();
 
     let gpio00 = PORT_00.split();
@@ -13,4 +14,34 @@ fn test_pin_set_high() {
     output.set_low();
 
     insta::assert_display_snapshot!(report.get_log());
+}
+
+#[test]
+fn test_pin_set_two_pins_same_port_high() {
+    let report = tracing::log::Report::new();
+
+    let gpio00 = PORT_00.split();
+    let mut p00_5 = gpio00.p00_5.into_push_pull_output();
+    let mut p00_6 = gpio00.p00_6.into_push_pull_output();
+
+    p00_5.set_high();
+    p00_6.set_high();
+
+    insta::assert_display_snapshot!(report.get_log());
+}
+
+
+#[test]
+fn test_pin_set_two_pins_on_two_ports_high() {
+    // let report = tracing::log::Report::new();
+    //
+    // let gpio00 = PORT_00.split();
+    // let gpio01 = PORT_01.split();
+    // let mut p00_5 = gpio00.p00_5.into_push_pull_output();
+    // let mut p01_6 = gpio01.p01_6.into_push_pull_output();
+    //
+    // p00_5.set_high();
+    // p01_6.set_high();
+    //
+    // insta::assert_display_snapshot!(report.get_log());
 }
