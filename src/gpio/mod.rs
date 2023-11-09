c8970d6a28170ea4395d94f5c9e7b592b8f49a30
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-11-09 12:06:49 +0100

    Simplify gpio! macro

--- src/gpio/mod.rs
+++ src/gpio/mod.rs
@@ -582,7 +582,7 @@ impl<const P: usize, const N: u8, MODE> Pin<P, N, MODE>
 }
 
 macro_rules! gpio {
-    ($PORTX:ty, $gpiox:ident, $port_id:expr, $PXn:ident, [
+    ($gpiox:ident, $PORTX:ty, $port_id:expr, $PXn:ident, [
         $($PXi:ident: ($pxi:ident, $i:expr, [$($A:literal),*] $(, $MODE:ty)?),)+
     ]) => {
         /// GPIO
@@ -643,12 +643,12 @@ use gpio;
 // cargo expand --lib --tests gpio::f4 --features stm32f401
 // The I removed all pins except one
 
-gpio!(crate::pac::port_00::Port00, gpio00, 0, P00n, [
+gpio!(gpio00, crate::pac::port_00::Port00, 0, P00n, [
     P00_5: (p00_5, 5, [1]),
     P00_6: (p00_6, 6, [1]),
 ]);
 
-gpio!(crate::pac::port_01::Port01, gpio01, 1, P01n, [
+gpio!(gpio01, crate::pac::port_01::Port01, 1, P01n, [
     P01_9: (p01_9, 9, [1]),
     P01_10: (p01_10, 10, [1]),
 ]);
