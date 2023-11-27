fbb9505c3d76d5e87fcef1e95f09e981c45f4a1d
anna.bonaldo <anna.bonaldo@bluewind.it>
2023-11-27 11:53:05 +0100

    CanNode tait

---
+++ examples/canky.rs
@@ -0,0 +1,107 @@
+//! Blinks an LED
+
+#![cfg_attr(target_arch = "tricore", no_main)]
+#![cfg_attr(target_arch = "tricore", no_std)]
+
+#[cfg(target_arch = "tricore")]
+tc37x_rt::entry!(main);
+#[cfg(target_arch = "tricore")]
+use defmt::println;
+
+use core::arch::asm;
+use tc37x_hal::cpu::asm;
+use tc37x_hal::gpio::GpioExt;
+use tc37x_hal::pac;
+use tc37x_pac::RegisterValue;
+use tc37x_hal::ssw::init_software; 
+use tc37x_hal::can::CanNodeHandler;
+
+pub enum State {
+    NotChanged = 0,
+    High = 1,
+    Low = 1 << 16,
+    Toggled = (1 << 16) | 1,
+}
+
+fn port_00_set_state(index: usize, state: State) {
+    let state = state as u32;
+    unsafe {
+        pac::PORT_00
+            .omr()
+            .init(|r| r.set_raw((state << index).into()));
+    };
+}
+
+fn port_00_set_mode(index: usize, mode: u32) {
+    let ioc_index = index / 4;
+    let shift = (index & 0x3) * 8;
+
+    let iocr: pac::Reg<pac::port_00::Iocr0, pac::RW> = unsafe {
+        let iocr0 = pac::PORT_00.iocr0();
+        let addr: *mut u32 = core::mem::transmute(iocr0);
+        let addr = addr.add(ioc_index as _);
+        core::mem::transmute(addr)
+    };
+
+    use tc37x_pac::common::hidden::RegValue;
+
+    unsafe {
+        iocr.modify_atomic(|mut r| {
+            *r.data_mut_ref() = (mode) << shift;
+            *r.get_mask_mut_ref() = 0xFFu32 << shift;
+            r
+        })
+    };
+}
+
+fn main() -> ! {
+    #[cfg(not(target_arch = "tricore"))]
+    let _report = tc37x_hal::tracing::print::Report::new();
+
+    println!("Start example: CanKy");
+
+    //ssw::init_software();
+    tc37x_hal::cpu::asm::enable_interrupts();
+
+    let gpio00 = pac::PORT_00.split();
+
+    let mut led1 = gpio00.p00_5.into_push_pull_output();
+    let mut led2 = gpio00.p00_6.into_push_pull_output();
+    let mut i = 0;
+    let can_module = CanNodeHandler::new(); 
+
+    //let can_transmission = CanCommunication::new(); 
+    //init_mcan();
+    //transmit_message();
+
+    loop {
+        led1.set_low();
+        led2.set_high();
+        wait_nop(100000);
+
+
+        println!("running {}", i);
+        i += 1;
+
+        led1.set_high();
+        led2.set_low();
+        wait_nop(100000);
+    }
+}
+
+fn wait_nop(cycle: u32) {
+    for _ in 0..cycle {
+        unsafe { asm!("nop") };
+    }
+}
+
+/* (TODO) to be moved in a separate module! (annabo) */
+
+
+pub struct CanCommunication; 
+
+impl CanCommunication{
+    fn new(&self) -> Self{
+        Self{}
+    }
+}
--- src/can/can.rs
+++ src/can/can.rs
@@ -1,156 +1,120 @@
-// use tc37x_pac::{self as pac};
-// use tc37x_pac::{can*}
+// CanNode trait impl to be moved on a separate file (annabo)
+pub trait CanNode {
+    fn node_id(&self) -> u8;
+    fn is_enabled(&self) -> bool;
+    fn is_suspended(&self) -> bool;
 
-// pub enum CanNumber {
-//     _0,
-//     _1,
-// }
+    fn enable_module(&self); 
+    fn disable_module(&self);
+    fn reset_module(&self);
+    //fn set_clock_source(&self, clock_select: ClockSelect, clock_source: ClockSource); 
+}
 
-// #[derive(Clone, Copy)]
-// pub struct Can {
-//     inner: pac::can0::Can0,
-// }
+use tc37x_pac::can0::Can0;
+use tc37x_pac::can1::Can1;
+use crate::scu; 
 
-// impl Can {
-//     pub const fn new(number: CanNumber) -> Self {
-//         let inner: pac::can0::Can0 = unsafe {
-//             match number {
-//                 CanNumber::_0 => pac::CAN0,
-//                 CanNumber::_1 => core::mem::transmute(pac::CAN1),
-//             }
-//         };
+impl CanNode for Can0 {
+    fn node_id(&self) -> u8 {
+        0
+    }
+    #[inline]
+    fn is_enabled(&self) -> bool {
+        !unsafe { tc37x_pac::CAN0.clc().read() }.diss().get()
+    }
 
-//         Self { inner }
+    fn is_suspended(&self) -> bool {
+        unsafe { tc37x_pac::CAN0.ocs().read() }.sussta().get()
+    }
+    fn enable_module(&self) {
+        let passw = scu::wdt::get_cpu_watchdog_password();
+        #[cfg(feature = "log")]
+        println!("enable module watchdog passw: {:x}", passw);
+
+        scu::wdt::clear_cpu_endinit_inline(passw);
+
+        unsafe { tc37x_pac::CAN0.clc().modify_atomic(|r| r.disr().set(false)) };
+        while !self.is_enabled() {}
+
+        scu::wdt::set_cpu_endinit_inline(passw);
+    }
+
+
+    fn disable_module(&self) {
+        let passw = scu::wdt::get_cpu_watchdog_password();
+
+        scu::wdt::clear_cpu_endinit_inline(passw);
+        unsafe { tc37x_pac::CAN0.clc().modify(|r| r.disr().set(true)) };
+
+        while self.is_enabled() {}
+
+        scu::wdt::set_cpu_endinit_inline(passw);
+    }
+
+    fn reset_module(&self) {
+        let passw = scu::wdt::get_cpu_watchdog_password();
+
+        scu::wdt::clear_cpu_endinit_inline(passw);
+        unsafe { tc37x_pac::CAN0.krst0().modify(|r| r.rst().set(true)) };
+        unsafe { tc37x_pac::CAN0.krst1().modify(|r| r.rst().set(true)) };
+        scu::wdt::set_cpu_endinit_inline(passw);
+
+        while !unsafe { tc37x_pac::CAN0.krst0().read() }.rststat().get() {}
+
+        scu::wdt::clear_cpu_endinit_inline(passw);
+        unsafe { tc37x_pac::CAN0.krstclr().init(|r| r.clr().set(true)) };
+        scu::wdt::set_cpu_endinit_inline(passw);
+    }
+
+    // fn set_clock_source(&self, clock_select: ClockSelect, clock_source: ClockSource) {
+    //     let mut mcr = unsafe { tc37x_pac::CAN0.mcr().read() };
+    //     mcr = mcr.ccce().set(true).ci().set(true);
+
+    //     unsafe { tc37x_pac::CAN0.mcr().write(mcr) };
+
+    //     mcr = mcr.clksel(clock_select.into()).set(clock_source.into());
+    //     unsafe { tc37x_pac::CAN0.mcr().write(mcr) };
+
+    //     mcr = mcr.ccce().set(false).ci().set(false);
+    //     unsafe { tc37x_pac::CAN0.mcr().write(mcr) };
+    // }
+
+   
+}
+
+// TODO (annabo)
+// replace CAN0 with CAN1 on implementation above. TBD with macros
+// impl CanNode for Can1 {
+//     fn nodeId(&self) -> u8 {
+//         0
 //     }
-// }
-
-// // IfxLld_Can_Std_Module_Functions
-// impl Can {
-//     pub const fn get_ptr(&self) -> *mut u8 {
-//         unsafe { core::mem::transmute(self.inner) }
-//     }
-
 //     #[inline]
-//     pub fn is_enabled(&self) -> bool {
-//         !unsafe { self.inner.clc().read() }.diss().get()
+//     fn is_enabled(&self) -> bool {
+//         !unsafe { tc37x_pac::CAN1.clc().read() }.diss().get()
 //     }
-
-//     pub fn is_suspended(&self) -> bool {
-//         unsafe { self.inner.ocs().read() }.sussta().get()
-//     }
-
-//     pub fn get_node(&self, node_id: NodeId) -> Node {
-//         Node::new(node_id)
-//     }
-
-//     pub fn enable_module(&self) {
-//         let passw = scu::wdt::get_cpu_watchdog_password();
-//         #[cfg(feature = "log")]
-//         defmt::debug!("enable module watchdog passw: {:x}", passw);
-
-//         scu::wdt::clear_cpu_endinit_inline(passw);
-
-//         unsafe { self.inner.clc().modify_atomic(|r| r.disr().set(false)) };
-//         while !self.is_enabled() {}
-
-//         scu::wdt::set_cpu_endinit_inline(passw);
-//     }
-
-//     pub fn disable_module(&self) {
-//         let passw = scu::wdt::get_cpu_watchdog_password();
-
-//         scu::wdt::clear_cpu_endinit_inline(passw);
-//         unsafe { self.inner.clc().modify(|r| r.disr().set(true)) };
-
-//         while self.is_enabled() {}
-
-//         scu::wdt::set_cpu_endinit_inline(passw);
-//     }
-
-//     pub fn reset_module(&self) {
-//         let passw = scu::wdt::get_cpu_watchdog_password();
-
-//         scu::wdt::clear_cpu_endinit_inline(passw);
-//         unsafe { self.inner.krst0().modify(|r| r.rst().set(true)) };
-//         unsafe { self.inner.krst1().modify(|r| r.rst().set(true)) };
-//         scu::wdt::set_cpu_endinit_inline(passw);
-
-//         while !unsafe { self.inner.krst0().read() }.rststat().get() {}
-
-//         scu::wdt::clear_cpu_endinit_inline(passw);
-//         unsafe { self.inner.krstclr().init(|r| r.clr().set(true)) };
-//         scu::wdt::set_cpu_endinit_inline(passw);
-//     }
-
-//     pub fn set_clock_source(&self, clock_select: ClockSelect, clock_source: ClockSource) {
-//         let mut mcr = unsafe { self.inner.mcr().read() };
-//         mcr = mcr.ccce().set(true).ci().set(true);
-
-//         unsafe { self.inner.mcr().write(mcr) };
-
-//         mcr = mcr.clksel(clock_select.into()).set(clock_source.into());
-//         unsafe { self.inner.mcr().write(mcr) };
-
-//         mcr = mcr.ccce().set(false).ci().set(false);
-//         unsafe { self.inner.mcr().write(mcr) };
+//     fn is_suspended(&self) -> bool {
+//         unsafe { tc37x_pac::CAN1.ocs().read() }.sussta().get()
 //     }
 // }
 
-// impl Can {
-//     #[allow(unused)]
-//     pub fn get_src(&self, line: InterruptLine) -> Src {
-//         let can_idx = unsafe {
-//             let can0: usize = core::mem::transmute(pac::CAN0);
-//             let can1: usize = core::mem::transmute(pac::CAN1);
-//             let current: usize = core::mem::transmute(self.inner);
-//             match current {
-//                 can0 => 0,
-//                 can1 => 1,
-//                 _ => unimplemented!(),
-//             }
-//         };
-//         let can_src_off: usize = src::offsets::CAN * (1 + can_idx);
-//         Src::new(can_src_off + line.0 as usize * 4)
-//     }
-// }
+pub struct CanNodeHandler<T: CanNode> {
+    node_id: u8,
+    node: T,
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+impl CanNodeHandler<Can0> {
+    pub const fn new() -> Self {
+        Self {
+            node_id: 0,
+            node: tc37x_pac::CAN0,
+        }
+    }
 
-//     #[test]
-//     pub fn test_set_clock_source_reg() {
-//         use core::mem::size_of_val;
-//         use pac::RegValue;
-//         extern crate std;
-//         use crate::reporter::*;
-
-//         let reporter = LogEffectReporter::default();
-//         pac::tracing::set_effect_reporter(std::boxed::Box::new(reporter.clone()));
-
-//         let module = Can::new(CanNumber::_0);
-//         let clock_select = ClockSelect::_0;
-//         let clock_source = ClockSource::Both;
-//         module.set_clock_source(clock_select, clock_source);
-
-//         let logs = reporter.get_logs();
-//         let expected_data = pac::can0::Mcr::default()
-//             .ccce()
-//             .set(true)
-//             .ci()
-//             .set(true)
-//             .clksel(clock_select.into())
-//             .set(clock_source.into())
-//             .data();
-
-//         assert_eq!(logs.len(), 4);
-//         assert_eq!(
-//             logs[2],
-//             ReportData::new(
-//                 ReportAction::Write(expected_data),
-//                 pac::CAN0.mcr().ptr() as usize,
-//                 size_of_val(&expected_data)
-//             )
-//         );
-//     }
-// }
+    pub fn init_module(&self) {
+        if !self.node.is_enabled() {
+            #[cfg(feature = "log")]
+            defmt::debug!("module was disabled, enabling it");
+            self.node.enable_module();
+        }
+    }
+}
