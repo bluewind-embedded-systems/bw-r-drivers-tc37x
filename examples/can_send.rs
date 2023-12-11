e7018857b0ca7b1def92ff2ebeafca35a5409093
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-12-11 12:00:15 +0100

    Add can example

--- examples/can_send.rs
+++ examples/can_send.rs
@@ -5,6 +5,7 @@
 
 #[cfg(target_arch = "tricore")]
 tc37x_rt::entry!(main);
+
 #[cfg(target_arch = "tricore")]
 use defmt::println;
 
@@ -19,40 +20,39 @@ fn main() -> ! {
     #[cfg(not(target_arch = "tricore"))]
     let _report = tc37x_hal::tracing::print::Report::new();
 
-    println!("Start example: CanKy");
+    println!("Start example: can_send");
 
     println!("Enable interrupts");
     enable_interrupts();
 
     let gpio00 = pac::PORT_00.split();
-
     let mut led1 = gpio00.p00_5.into_push_pull_output();
 
     println!("Create can module ... ");
 
-    // cannot create a module if already taken
-    let can_module = CanModule0::new(); // get an enabled module
-    println!("Can module init .. ");
-    can_module.init_module();
+    let can_module = CanModule0::default();
+    let can_module_config = CanModuleConfig::default();
+    let can_module = can_module.init(can_module_config)?;
 
-    //ottengo un nodo
-    //let node: uninitializedNode = can_module.get_node(0);
-    //let initedNode: InitedNode = node.init(config{configuration constructor})
+    let can_node = can_module.get_node(0)?;
+    let can_node_config = CanNodeConfig::default();
+    let can_node = can_node.init(can_node_config)?;
 
-    // cannot take a node if already taken
-    // can_module.get_node(0);
+    let mut data: [u8; 8] = [0; 8];
 
-    // let msg_id =  0;
-    // let msg = Message::TxMessage::new(msg_id)
-    // node.send_message(msg);
-
-    if can_module.is_enabled() {
-        println!("Can module is now enabled! ")
-    } else {
-        println!("Can module NOT enabled! Something went wrong!")
-    }
+    let can = can_node;
+    let mut count = 0;
 
     loop {
+        if count < 255 {
+            count += 1;
+        } else {
+            count = 0;
+        }
+        data[0] = count;
+
+        let test_frame = Frame::new(0x0CFE6E00, data);
+        can.transmit(&test_frame);
         led1.set_high();
         wait_nop(100000);
         led1.set_low();
@@ -63,15 +63,5 @@ fn main() -> ! {
 fn wait_nop(cycle: u32) {
     for _ in 0..cycle {
         unsafe { core::arch::asm!("nop") };
-    }
-}
-
-/* (TODO) to be moved in a separate module! (annabo) */
-
-pub struct CanCommunication;
-
-impl CanCommunication {
-    fn new(&self) -> Self {
-        Self {}
     }
 }
