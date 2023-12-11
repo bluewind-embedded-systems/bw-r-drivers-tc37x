17d6e96b010b2d3719dc449a1b7d3307810d4303
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-12-11 11:09:49 +0100

    Add tools/build_on_host tools/build_on_target tools/run_on_host tools/run_on_target

--- compile_on_target.sh
+++
@@ -1,6 +0,0 @@
-
-cp .cargo/config.toml.target .cargo/config.toml
-cp rust-toolchain.toml.target rust-toolchain.toml
-
-export DEFMT_LOG="trace"
-cargo build --example=canky --features=example
--- run_on_host.sh
+++
@@ -1,4 +0,0 @@
-cp .cargo/config.toml.host .cargo/config.toml
-cp rust-toolchain.toml.host rust-toolchain.toml
-
-cargo run --example=canky --features=tracing
