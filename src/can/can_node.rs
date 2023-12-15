85f5cce8194fba685604fde6bc582680a8aff512
anna.bonaldo <anna.bonaldo@bluewind.it>
2023-12-15 10:57:59 +0100

    reuse all compilig code

--- src/can/can_node.rs
+++ src/can/can_node.rs
@@ -53,20 +53,20 @@ impl NodeId {
     }
 }
 
+
 pub struct CanNode {
     module: CanModule,
     node_id: NodeId,
-    inner: tc37x_pac::can0::Node,
 }
 
 impl CanNode {
     /// Only a module can create a node. This function is only accessible from within this crate.
     pub(crate) fn new(module: CanModule, node_id: NodeId) -> Self {
-        let inner = module.registers().node(node_id.0.into());
+
+        //let inner = module.registers().node(node_id.0.into());
         Self {
             module,
             node_id,
-            inner,
         }
     }
 
@@ -98,7 +98,7 @@ impl CanNode {
 
     #[inline]
     fn enable_configuration_change(&self) {
-        let cccr = self.inner.cccr();
+        let cccr = tc37x_pac::CAN0.cccr0();
 
         if unsafe { cccr.read() }.init().get() {
             unsafe { cccr.modify(|r| r.cce().set(false)) };
@@ -163,7 +163,7 @@ impl CanNode {
 
     fn set_bit_timing(&self, timing: BitTiming) {
         unsafe {
-            self.inner.nbtp().modify(|r| {
+            tc37x_pac::CAN0.nbtp0().modify(|r| {
                 r.nbrp()
                     .set(timing.brp)
                     .nsjw()
@@ -178,7 +178,7 @@ impl CanNode {
 
     fn set_bit_timing_values(&self, sjw: u8, time_segment2: u8, time_segment1: u8, prescaler: u16) {
         unsafe {
-            self.inner.nbtp().modify(|r| {
+            tc37x_pac::CAN0.nbtp0().modify(|r| {
                 r.nsjw()
                     .set(sjw)
                     .ntseg1()
@@ -195,7 +195,7 @@ impl CanNode {
         let timing = calculate_fast_bit_timing(module_freq, baudrate, sample_point, sjw);
 
         unsafe {
-            self.inner.dbtp().modify(|r| {
+            tc37x_pac::CAN0.dbtp0().modify(|r| {
                 r.dbrp()
                     .set(timing.brp.try_into().unwrap())
                     .dsjw()
@@ -213,10 +213,9 @@ impl CanNode {
         sjw: u8,
         time_segment2: u8,
         time_segment1: u8,
-        prescaler: u8,
-    ) {
+        prescaler: u8,) {
         unsafe {
-            self.inner.dbtp().modify(|r| {
+            tc37x_pac::CAN0.dbtp0().modify(|r| {
                 r.dsjw()
                     .set(sjw)
                     .dtseg1()
@@ -230,7 +229,457 @@ impl CanNode {
     }
 
     pub fn set_transceiver_delay_compensation_offset(&self, delay: u8) {
-        unsafe { self.inner.dbtp().modify(|r| r.tdc().set(true)) };
-        unsafe { self.inner.tdcr().modify(|r| r.tdco().set(delay)) };
+        unsafe { tc37x_pac::CAN0.dbtp0().modify(|r| r.tdc().set(true)) };
+        unsafe { tc37x_pac::CAN0.tdcr0().modify(|r| r.tdco().set(delay)) };
+    }
+
+    fn  set_tx_buffer_data_field_size(){
+        todo!();
     }
 }
+
+
+// IfxLld_Can_Std_Rx_Element_Functions
+impl CanNode {
+    pub fn get_rx_fifo0_fill_level(&self) -> u8 {
+        unsafe { tc37x_pac::CAN0.rxf0s0().read() }.f0fl().get()
+    }
+
+    // pub fn get_rx_fifo0_get_index(&self) -> RxBufferId {
+    //     let id = unsafe { tc37x_pac::CAN0.rxf0s0().read() }.f0gi().get();
+    //     RxBufferId::new_const(id)
+    // }
+
+    pub fn get_rx_fifo1_fill_level(&self) -> u8 {
+        unsafe { tc37x_pac::CAN0.rxf1s0().read() }.f1fl().get()
+    }
+
+    // pub fn get_rx_fifo1_get_index(&self) -> RxBufferId {
+    //     let id = unsafe { tc37x_pac::CAN0.rxf1s0().read() }.f1gi().get();
+    //     RxBufferId::new_const(id)
+    // }
+
+    // #[inline]
+    // pub fn set_rx_buffer_data_field_size(&self, size: DataFieldSize) {
+    //     unsafe { tc37x_pac::CAN0.rxesc0().modify(|r| r.rbds().set(size.into())) };
+    // }
+
+    // pub fn is_rx_buffer_new_data_updated(&self, rx_buffer_id: RxBufferId) -> bool {
+    //     let (data, mask) = if rx_buffer_id < RxBufferId::new_const(32) {
+    //         let data = unsafe { tc37x_pac::CAN0.ndat10().read() }.data();
+    //         let mask = 1 << u8::from(rx_buffer_id);
+    //         (data, mask)
+    //     } else {
+    //         let data = unsafe { tc37x_pac::CAN0.ndat20().read() }.data();
+    //         let mask = 1 << (u8::from(rx_buffer_id) - 32);
+    //         (data, mask)
+    //     };
+    //     (data & mask) != 0
+    // }
+
+    // #[inline]
+    // pub fn set_rx_fifo0_acknowledge_index(&self, rx_buffer_id: RxBufferId) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .rxf0a0()
+    //             .modify(|r| r.f0ai().set(rx_buffer_id.into()))
+    //     };
+    // }
+
+    // #[inline]
+    // pub fn set_rx_fifo1_acknowledge_index(&self, rx_buffer_id: RxBufferId) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .rxf1a0()
+    //             .modify(|r| r.f1ai().set(rx_buffer_id.into()))
+    //     };
+    // }
+
+    // pub fn clear_rx_buffer_new_data_flag(&self, rx_buffer_id: RxBufferId) {
+    //     if rx_buffer_id < RxBufferId::new_const(32) {
+    //         unsafe {
+    //             tc37x_pac::CAN0.ndat10().init(|mut r| {
+    //                 *r.data_mut_ref() = 1u32 << u8::from(rx_buffer_id);
+    //                 r
+    //             })
+    //         };
+    //     } else {
+    //         unsafe {
+    //             tc37x_pac::CAN0.ndat20().init(|mut r| {
+    //                 *r.data_mut_ref() = 1u32 << (u8::from(rx_buffer_id) - 32);
+    //                 r
+    //             })
+    //         };
+    //     }
+    // }
+
+    #[inline]
+    pub fn set_rx_buffers_start_address(&self, address: u16) {
+        unsafe { tc37x_pac::CAN0.rxbc0().modify(|r| r.rbsa().set(address >> 2)) };
+    }
+
+    // #[inline]
+    // pub fn set_rx_fifo0_data_field_size(&self, size: DataFieldSize) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .rxesc0()
+    //             .modify(|r| r.f0ds().set(can0::node::rxesc::F0Ds(size.into())))
+    //     };
+    // }
+
+    // #[inline]
+    // pub fn set_rx_fifo0_operating_mode(&self, mode: RxFifoMode) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .rxf0c0()
+    //             .modify(|r| r.f0om().set(mode == RxFifoMode::Overwrite))
+    //     };
+    // }
+
+    #[inline]
+    pub fn set_rx_fifo0_size(&self, size: u8) {
+        unsafe { tc37x_pac::CAN0.rxf0c0().modify(|r| r.f0s().set(size)) };
+    }
+
+    #[inline]
+    pub fn set_rx_fifo0_start_address(&self, address: u16) {
+        unsafe { tc37x_pac::CAN0.rxf0c0().modify(|r| r.f0sa().set(address >> 2)) };
+    }
+
+    #[inline]
+    pub fn set_rx_fifo0_watermark_level(&self, level: u8) {
+        unsafe { tc37x_pac::CAN0.rxf0c0().modify(|r| r.f0wm().set(level)) };
+    }
+
+    // #[inline]
+    // pub fn set_rx_fifo1_data_field_size(&self, size: DataFieldSize) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .rxesc0()
+    //             .modify(|r| r.f1ds().set(can0::node::rxesc::F1Ds(size.into())))
+    //     };
+    // }
+
+    // #[inline]
+    // pub fn set_rx_fifo1_operating_mode(&self, mode: RxFifoMode) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .rxf1c0()
+    //             .modify(|r| r.f1om().set(mode == RxFifoMode::Overwrite))
+    //     };
+    // }
+
+    #[inline]
+    pub fn set_rx_fifo1_size(&self, size: u8) {
+        unsafe { tc37x_pac::CAN0.rxf1c0().modify(|r| r.f1s().set(size)) };
+    }
+
+    #[inline]
+    pub fn set_rx_fifo1_start_address(&self, address: u16) {
+        unsafe { tc37x_pac::CAN0.rxf1c0().modify(|r| r.f1sa().set(address >> 2)) };
+    }
+
+    #[inline]
+    pub fn set_rx_fifo1_watermark_level(&self, level: u8) {
+        unsafe { tc37x_pac::CAN0.rxf1c0().modify(|r| r.f1wm().set(level)) };
+    }
+
+}
+
+// IfxLld_Can_Std_Tx_Element_Functions
+impl CanNode {
+    // #[inline]
+    // pub fn get_tx_fifo_queue_put_index(&self) -> TxBufferId {
+    //     let id = unsafe { tc37x_pac::CAN0.txfqs().read() }.tfqpi().get();
+    //     TxBufferId::new_const(id)
+    // }
+
+    // #[inline]
+    // pub fn is_tx_buffer_cancellation_finished(&self, tx_buffer_id: TxBufferId) -> bool {
+    //     self.is_tx_buffer_transmission_occured(tx_buffer_id)
+    // }
+
+    // #[inline]
+    // pub fn is_tx_buffer_request_pending(&self, tx_buffer_id: TxBufferId) -> bool {
+    //     unsafe { tc37x_pac::CAN0.txbrp0().read() }
+    //         .trp(tx_buffer_id.into())
+    //         .get()
+    // }
+
+    // #[inline]
+    // pub fn is_tx_buffer_transmission_occured(&self, tx_buffer_id: TxBufferId) -> bool {
+    //     let data = unsafe { tc37x_pac::CAN0.txbto0().read() }.data();
+    //     let mask = 1u32 << u32::from(tx_buffer_id);
+    //     (data & mask) != 0
+    // }
+
+    #[inline]
+    pub fn is_tx_event_fifo_element_lost(&self) -> bool {
+        unsafe { tc37x_pac::CAN0.txefs0().read() }.tefl().get()
+    }
+
+    #[inline]
+    pub fn is_tx_event_fifo_full(&self) -> bool {
+        unsafe { tc37x_pac::CAN0.txefs0().read() }.eff().get()
+    }
+
+    #[inline]
+    pub fn is_tx_fifo_queue_full(&self) -> bool {
+        unsafe { tc37x_pac::CAN0.txfqs0().read() }.tfqf().get()
+    }
+
+    #[inline]
+    pub fn pause_trasmission(&self, enable: bool) {
+        unsafe { tc37x_pac::CAN0.cccr0().modify(|r| r.txp().set(enable)) };
+    }
+
+    #[inline]
+    pub fn set_dedicated_tx_buffers_number(&self, number: u8) {
+        unsafe { tc37x_pac::CAN0.txbc0().modify(|r| r.ndtb().set(number)) };
+    }
+
+    #[inline]
+    // pub fn set_tx_buffer_add_request(&self, tx_buffer_id: TxBufferId) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .txbar0()
+    //             .modify(|r| r.ar(tx_buffer_id.into()).set(true))
+    //     }
+    // }
+
+    // #[inline]
+    // pub fn set_tx_buffer_data_field_size(&self, data_field_size: DataFieldSize) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .txesc0()
+    //             .modify(|r| r.tbds().set(data_field_size.into()))
+    //     };
+    // }
+
+    #[inline]
+    pub fn set_tx_buffer_start_address(&self, address: u16) {
+        unsafe { tc37x_pac::CAN0.txbc0().modify(|r| r.tbsa().set(address >> 2)) };
+    }
+
+    #[inline]
+    // pub fn set_transmit_fifo_queue_mode(&self, mode: TxMode) {
+    //     if let TxMode::Fifo | TxMode::Queue = mode {
+    //         let val = (mode as u8) != 0;
+    //         unsafe { tc37x_pac::CAN0.txbc0().modify(|r| r.tfqm().set(val)) };
+    //     } else {
+    //         panic!("invalid fifo queue mode");
+    //     }
+    // }
+
+    #[inline]
+    pub fn set_transmit_fifo_queue_size(&self, number: u8) {
+        unsafe { tc37x_pac::CAN0.txbc0().modify(|r| r.tfqs().set(number)) };
+    }
+
+    // pub fn get_data_field_size(&self, from: ReadFrom) -> u8 {
+    //     let rx_esc = unsafe { tc37x_pac::CAN0.rxesc0().read() };
+    //     let size_code = match from {
+    //         ReadFrom::Buffer(_) => rx_esc.rbds().get().0,
+    //         ReadFrom::RxFifo0 => rx_esc.f0ds().get().0,
+    //         ReadFrom::RxFifo1 => rx_esc.f1ds().get().0,
+    //     };
+
+    //     if size_code < DataFieldSize::_32.into() {
+    //         (size_code + 2) * 4
+    //     } else {
+    //         (size_code - 3) * 16
+    //     }
+    // }
+    // pub fn get_tx_buffer_data_field_size(&self) -> u8 {
+    //     let size_code = unsafe { tc37x_pac::CAN0.txesc0().read() }.tbds().get().0;
+
+    //     if size_code < DataFieldSize::_32.into() {
+    //         (size_code + 2) * 4
+    //     } else {
+    //         (size_code - 3) * 16
+    //     }
+    // }
+
+    // pub fn get_rx_element_address(
+    //     &self,
+    //     ram_base_address: u32,
+    //     tx_buffers_start_address: u16,
+    //     buf_from: ReadFrom,
+    //     buffer_number: RxBufferId,
+    // ) -> Rx {
+    //     let num_of_config_bytes = 8u32;
+    //     let num_of_data_bytes = self.get_data_field_size(buf_from) as u32;
+    //     let tx_buffer_size = num_of_config_bytes + num_of_data_bytes;
+    //     let tx_buffer_index = tx_buffer_size * u32::from(buffer_number);
+
+    //     let tx_buffer_element_address =
+    //         ram_base_address + tx_buffers_start_address as u32 + tx_buffer_index;
+
+    //     Rx::new(tx_buffer_element_address as *mut u8)
+    // }
+
+    // pub fn get_tx_element_address(
+    //     &self,
+    //     ram_base_address: u32,
+    //     tx_buffers_start_address: u16,
+    //     buffer_number: TxBufferId,
+    // ) -> Tx {
+    //     let num_of_config_bytes = 8u32;
+    //     let num_of_data_bytes = self.get_tx_buffer_data_field_size() as u32;
+    //     let tx_buffer_size = num_of_config_bytes + num_of_data_bytes;
+    //     let tx_buffer_index = tx_buffer_size * u32::from(buffer_number);
+
+    //     let tx_buffer_element_address =
+    //         ram_base_address + tx_buffers_start_address as u32 + tx_buffer_index;
+
+    //     Tx::new(tx_buffer_element_address as *mut u8)
+    // }
+}
+
+// IfxLld_Can_Std_Tx_Event_FIFO_Element_Functions
+impl CanNode {
+    #[inline]
+    pub fn set_tx_event_fifo_start_address(&self, address: u16) {
+        unsafe { tc37x_pac::CAN0.txefc0().modify(|r| r.efsa().set(address >> 2)) };
+    }
+
+    #[inline]
+    pub fn set_tx_event_fifo_size(&self, size: u8) {
+        unsafe { tc37x_pac::CAN0.txefc0().modify(|r| r.efs().set(size)) };
+    }
+}
+
+// IfxLld_Can_Std_Interrupt_Functions
+impl CanNode {
+    // #[inline]
+    // pub fn enable_tx_buffer_transmission_interrupt(&self, tx_buffer_id: TxBufferId) {
+    //     unsafe {
+    //         tc37x_pac::CAN0.txbtie0().modify(|mut r| {
+    //             *r.data_mut_ref() |= 1 << tx_buffer_id.0;
+    //             r
+    //         })
+    //     };
+    // }
+
+    // pub fn set_group_interrupt_line(
+    //     &self,
+    //     interrupt_group: InterruptGroup,
+    //     interrupt_line: InterruptLine,
+    // ) {
+    //     if interrupt_group <= InterruptGroup::Loi {
+    //         unsafe {
+    //             tc37x_pac::CAN0.grint10().modify(|mut r| {
+    //                 *r.data_mut_ref() |= (interrupt_line.0 as u32) << (interrupt_group as u32 * 4);
+    //                 r
+    //             })
+    //         };
+    //     } else {
+    //         unsafe {
+    //             tc37x_pac::CAN0.grint20().modify(|mut r| {
+    //                 *r.data_mut_ref() |=
+    //                     (interrupt_line.0 as u32) << ((interrupt_group as u32 % 8) * 4);
+    //                 r
+    //             })
+    //         };
+    //     }
+    // }
+
+    // #[inline]
+    // pub fn enable_interrupt(&self, interrupt: Interrupt) {
+    //     unsafe {
+    //         tc37x_pac::CAN0.ie0().modify(|mut r| {
+    //             *r.data_mut_ref() |= 1 << interrupt as u32;
+    //             r
+    //         })
+    //     };
+    // }
+
+    // #[inline]
+    // pub fn clear_interrupt_flag(&self, interrupt: Interrupt) {
+    //     unsafe {
+    //         tc37x_pac::CAN0.ir0().init(|mut r| {
+    //             *r.data_mut_ref() = 1 << interrupt as u32;
+    //             r
+    //         })
+    //     };
+    // }
+}
+
+// IfxLld_Can_Std_Filter_Functions
+impl CanNode {
+    #[inline]
+    pub fn set_standard_filter_list_start_address(&self, address: u16) {
+        unsafe { tc37x_pac::CAN0.sidfc0().modify(|r| r.flssa().set(address >> 2)) };
+    }
+
+    #[inline]
+    pub fn set_standard_filter_list_size(&self, size: u8) {
+        unsafe { tc37x_pac::CAN0.sidfc0().modify(|r| r.lss().set(size)) };
+    }
+
+   //#[inline]
+    // pub fn configure_standard_filter_for_non_matching_frame(&self, filter: NonMatchingFrame) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .gfc0()
+    //             .modify(|r| r.anfs().set(can0::node::gfc::Anfs(filter as _)))
+    //     };
+    // }
+
+    #[inline]
+    pub fn reject_remote_frames_with_standard_id(&self) {
+        unsafe { tc37x_pac::CAN0.gfc0().modify(|r| r.rrfs().set(true)) };
+    }
+
+    #[inline]
+    pub fn set_extended_filter_list_start_address(&self, address: u16) {
+        unsafe { tc37x_pac::CAN0.xidfc0().modify(|r| r.flesa().set(address >> 2)) };
+    }
+
+    #[inline]
+    pub fn set_extended_filter_list_size(&self, size: u8) {
+        unsafe { tc37x_pac::CAN0.xidfc0().modify(|r| r.lse().set(size)) };
+    }
+
+    #[inline]
+    // pub fn configure_extended_filter_for_non_matching_frame(&self, filter: NonMatchingFrame) {
+    //     unsafe {
+    //         tc37x_pac::CAN0
+    //             .gfc0()
+    //             .modify(|r| r.anfe().set(can0::node::gfc::Anfe(filter as _)))
+    //     };
+    // }
+
+    #[inline]
+    pub fn reject_remote_frames_with_extended_id(&self) {
+        unsafe { tc37x_pac::CAN0.gfc0().modify(|r| r.rrfe().set(true)) };
+    }
+}
+
+// pub fn get_standard_filter_element_address(
+//     ram_base_address: u32,
+//     standard_filter_list_start_address: u16,
+//     filter_number: u8,
+// ) -> StdMsg {
+//     let filter_index = filter_number as u32 * 4;
+//     let standard_filter_element_address =
+//         ram_base_address + standard_filter_list_start_address as u32 + filter_index;
+//     StdMsg::new(standard_filter_element_address as _)
+// }
+
+// pub fn get_extended_filter_element_address(
+//     ram_base_address: u32,
+//     extended_filter_list_start_address: u16,
+//     filter_number: u8,
+// ) -> ExtMsg {
+//     let filter_index = filter_number as u32 * 8;
+//     let extended_filter_element_address =
+//         ram_base_address + extended_filter_list_start_address as u32 + filter_index;
+//     ExtMsg::new(extended_filter_element_address as _)
+// }
+
+// impl GroupInterruptConfig {
+//     pub fn is_enable_src(&self) -> bool {
+//         self.priority > 0 || self.type_of_service == Tos::Dma
+//     }
+// }
