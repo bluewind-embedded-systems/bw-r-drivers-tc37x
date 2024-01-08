cdf86a4a3f8d58dac5186cfb63d01aa744a1c109
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-01-08 16:09:42 +0100

    Fix log message

--- src/ssw/tc0.rs
+++ src/ssw/tc0.rs
@@ -1,15 +1,14 @@
 use crate::scu::ccu;
+use crate::log::debug;
 
 use super::infra::is_application_reset;
 
 pub fn init_software() {
     if !is_application_reset() {
-        #[cfg(feature = "log")]
-        defmt::debug!("power on reset");
+        debug!("power on reset");
         // TODO Handle error
         let _ = ccu::init(&ccu::DEFAULT_CLOCK_CONFIG);
     } else {
-        #[cfg(feature = "log")]
-        defmt::debug!("application reset")
+        debug!("application reset")
     }
 }
