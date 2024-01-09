16f7bcbcebf5e1334e887e88950fdc860cea36e5
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-01-09 11:28:37 +0100

    Add HexSlice for printing of memory chunks as a hex string

--- src/log.rs
+++ src/log.rs
@@ -50,3 +50,42 @@ macro_rules! no_log_warn {
 macro_rules! no_log_error {
     ($($ignored:tt)*) => {};
 }
+
+/// A wrapper around a slice of bytes that implements `Display` to print the
+/// bytes as a hex string.
+///
+/// ```
+/// use tc37x_hal::log::HexSlice;
+/// let bytes = &[1u8, 2, 3, 4];
+/// assert_eq!(format!("{}", HexSlice::from(bytes)), "01020304");
+/// ```
+pub struct HexSlice<'a>(&'a [u8]);
+
+impl core::fmt::Display for HexSlice<'_> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        for byte in self.0 {
+            write!(f, "{:02x}", byte)?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(feature = "log_with_defmt")]
+impl defmt::Format for HexSlice<'_> {
+    fn format(&self, f: defmt::Formatter) {
+        for byte in self.0 {
+            defmt::write!(f, "{:02x}", byte);
+        }
+    }
+}
+
+impl<'a, const N: usize> From<&'a [u8; N]> for HexSlice<'a> {
+    fn from(value: &'a [u8; N]) -> Self {
+        HexSlice(value.as_slice())
+    }
+}
+impl<'a> From<&'a [u8]> for HexSlice<'a> {
+    fn from(value: &'a [u8]) -> Self {
+        HexSlice(value)
+    }
+}
