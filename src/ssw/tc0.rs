d0250375aea6d46e57553e0879aba8abeb18ad51
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-01-15 16:45:03 +0100

    Return code from init_software

--- src/ssw/tc0.rs
+++ src/ssw/tc0.rs
@@ -1,14 +1,15 @@
 use crate::log::debug;
 use crate::scu::ccu;
+use crate::scu::ccu::InitError;
 
 use super::infra::is_application_reset;
 
-pub fn init_software() {
+pub fn init_software() -> Result<(), InitError> {
     if !is_application_reset() {
         debug!("power on reset");
-        // TODO Handle error
-        let _ = ccu::init(&ccu::DEFAULT_CLOCK_CONFIG);
+        ccu::init(&ccu::DEFAULT_CLOCK_CONFIG)
     } else {
         debug!("application reset");
+        Ok(())
     }
 }
