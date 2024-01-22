81be36c9328cbac01b398c5c58e6a930c588a4f9
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-01-22 11:07:00 +0100

    Fix for pac update: disable code needing SRC pac module

--- src/can/can_node/mod.rs
+++ src/can/can_node/mod.rs
@@ -339,30 +339,30 @@ impl NewCanNode<$Reg> {
     ) {
         self.set_group_interrupt_line(interrupt_group, line);
 
-        let src = tc37x_pac::SRC;
-
-        use crate::pac::{src::Can0Int0, Reg, RW};
-
-        let can_int: Reg<Can0Int0, RW> = match (self.module.id(), line) {
-            // TODO Add other lines and can modules
-            (ModuleId::Can0, InterruptLine(0)) => unsafe { transmute(src.can0int0()) },
-            (ModuleId::Can0, InterruptLine(1)) => unsafe { transmute(src.can0int1()) },
-            (ModuleId::Can1, InterruptLine(0)) => unsafe { transmute(src.can1int0()) },
-            (ModuleId::Can1, InterruptLine(1)) => unsafe { transmute(src.can1int1()) },
-            _ => unreachable!(),
-        };
-
+        // let src = tc37x_pac::SRC;
+        //
+        // use crate::pac::{Reg, RW};
+        //
+        // let can_int: Reg<Can0Int0, RW> = match (self.module.id(), line) {
+        //     // TODO Add other lines and can modules
+        //     (ModuleId::Can0, InterruptLine(0)) => unsafe { transmute(src.can0int0()) },
+        //     (ModuleId::Can0, InterruptLine(1)) => unsafe { transmute(src.can0int1()) },
+        //     (ModuleId::Can1, InterruptLine(0)) => unsafe { transmute(src.can1int0()) },
+        //     (ModuleId::Can1, InterruptLine(1)) => unsafe { transmute(src.can1int1()) },
+        //     _ => unreachable!(),
+        // };
+        //
         let priority = priority;
         let tos = tos as u8;
 
         // Set priority and type of service
-        unsafe { can_int.modify(|r| r.srpn().set(priority).tos().set(tos)) };
+        // unsafe { can_int.modify(|r| r.srpn().set(priority).tos().set(tos)) };
 
         // Clear request
-        unsafe { can_int.modify(|r| r.clrr().set(true)) };
+        // unsafe { can_int.modify(|r| r.clrr().set(true)) };
 
         // Enable service request
-        unsafe { can_int.modify(|r| r.sre().set(true)) };
+        // unsafe { can_int.modify(|r| r.sre().set(true)) };
 
         // Enable interrupt
         self.effects.enable_interrupt(interrupt);
