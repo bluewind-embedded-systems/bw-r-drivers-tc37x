95caad1a702ef17e791d40963169a236ac81d015
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-01-24 14:47:11 +0100

    Improve impl_can_module macro to accept module path

--- src/can/can_module.rs
+++ src/can/can_module.rs
@@ -22,7 +22,7 @@ pub struct Enabled;
 pub struct Module<Reg, State = Disabled>(PhantomData<(Reg, State)>);
 
 macro_rules! impl_can_module {
-    ($reg:ident, $m:ident, $Reg:ty, $id: expr) => {
+    ($reg:path, $($m:ident)::+, $Reg:ty, $id: expr) => {
         impl Module<$Reg, Disabled> {
             pub const fn new() -> Module<$Reg, Disabled> {
                 Module::<$Reg, Disabled>(PhantomData)
@@ -48,12 +48,12 @@ macro_rules! impl_can_module {
         }
 
         impl Module<$Reg, Enabled> {
-            pub fn take_node(&mut self, node_id: NodeId) -> Result<NewCanNode<$m::N, $Reg>, ()> {
+            pub fn take_node(&mut self, node_id: NodeId) -> Result<NewCanNode<$($m)::+::N, $Reg>, ()> {
                 // Instead of dealing with lifetimes, we just create a new instance of CanModule
                 // TODO This is not ideal, but it works for now
                 // TODO Remember the node has been taken and return None on next call
                 let module = Module::<$Reg, Enabled>(PhantomData);
-                Ok(Node::<$m::N, $Reg>::new(module, node_id))
+                Ok(Node::<$($m)::+::N, $Reg>::new(module, node_id))
             }
 
             pub fn id(&self) -> ModuleId {
@@ -65,16 +65,16 @@ macro_rules! impl_can_module {
                 clock_select: ClockSelect,
                 clock_source: ClockSource,
             ) -> Result<(), ()> {
-                use $m::mcr::{Ccce, Ci, Clksel0, Clksel1, Clksel2, Clksel3};
+                use $($m)::+::mcr::{Ccce, Ci, Clksel0, Clksel1, Clksel2, Clksel3};
 
                 let mcr = self.read_mcr();
 
                 // Enable CCCE and CI
                 let mcr = mcr
                     .ccce()
-                    .set($m::mcr::Ccce::CONST_11)
+                    .set($($m)::+::mcr::Ccce::CONST_11)
                     .ci()
-                    .set($m::mcr::Ci::CONST_11);
+                    .set($($m)::+::mcr::Ci::CONST_11);
                 self.write_mcr(mcr);
 
                 // Select clock
@@ -120,11 +120,11 @@ macro_rules! impl_can_module {
                 &$reg
             }
 
-            fn read_mcr(&self) -> $m::Mcr {
+            fn read_mcr(&self) -> $($m)::+::Mcr {
                 unsafe { $reg.mcr().read() }
             }
 
-            fn write_mcr(&self, mcr: $m::Mcr) {
+            fn write_mcr(&self, mcr: $($m)::+::Mcr) {
                 unsafe { $reg.mcr().write(mcr) }
             }
 
@@ -135,15 +135,8 @@ macro_rules! impl_can_module {
     };
 }
 
-use crate::pac::can0;
-use crate::pac::can0::Can0;
-use crate::pac::can1;
-use crate::pac::can1::Can1;
-use crate::pac::CAN0;
-use crate::pac::CAN1;
-
-impl_can_module!(CAN0, can0, Can0, ModuleId::Can0);
-impl_can_module!(CAN1, can1, Can1, ModuleId::Can1);
+impl_can_module!(pac::CAN0, pac::can0, pac::can0::Can0, ModuleId::Can0);
+impl_can_module!(pac::CAN1, pac::can1, pac::can1::Can1, ModuleId::Can1);
 
 pub(crate) struct ClockSelect(u8);
 
