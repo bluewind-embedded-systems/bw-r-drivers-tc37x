21587096581554461ca67095b61bb074b28109ca
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-02-19 12:26:01 +0100

    Remove useless Default explicit implementation

--- src/can/can_node/config.rs
+++ src/can/can_node/config.rs
@@ -12,25 +12,11 @@ pub struct NodeInterruptConfig {
     pub tos: Tos,
 }
 
+#[derive(Default)]
 pub struct NodeConfig {
     pub clock_source: ClockSource,
     pub baud_rate: BitTimingConfig,
     pub fast_baud_rate: FastBitTimingConfig,
     pub transceiver_delay_offset: u8,
     pub frame_mode: FrameMode,
-}
-
-// Note: the Default trait implementation must be explicitly defined because
-// the derive macro needs all generic parameters to implement Default, even
-// if it is not necessary.
-impl Default for NodeConfig {
-    fn default() -> Self {
-        Self {
-            clock_source: Default::default(),
-            baud_rate: Default::default(),
-            fast_baud_rate: Default::default(),
-            transceiver_delay_offset: 0,
-            frame_mode: Default::default(),
-        }
-    }
 }
