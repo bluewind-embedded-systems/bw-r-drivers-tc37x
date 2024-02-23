1400fa4b111d57f65a0351e3b54cbf8baec15730
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-02-23 16:25:13 +0100

    unpublish wdt_call functions

--- src/scu/wdt_call.rs
+++ src/scu/wdt_call.rs
@@ -1,20 +1,18 @@
 use crate::scu::wdt;
 
-// TODO Are we sure we want to publish this function?
-pub fn call_without_endinit<R>(f: impl FnOnce() -> R) -> R {
+#[allow(dead_code)]
+pub(crate) fn call_without_endinit<R>(f: impl FnOnce() -> R) -> R {
     call_without_cpu_endinit(|| call_without_safety_endinit(f))
 }
 
-// TODO Are we sure we want to publish this function?
-pub fn call_without_cpu_endinit<R>(f: impl FnOnce() -> R) -> R {
+pub(crate) fn call_without_cpu_endinit<R>(f: impl FnOnce() -> R) -> R {
     wdt::clear_cpu_endinit_inline();
     let result = f();
     wdt::set_cpu_endinit_inline();
     result
 }
 
-// TODO Are we sure we want to publish this function?
-pub fn call_without_safety_endinit<R>(f: impl FnOnce() -> R) -> R {
+pub(crate) fn call_without_safety_endinit<R>(f: impl FnOnce() -> R) -> R {
     wdt::clear_safety_endinit_inline();
     let result = f();
     wdt::set_safety_endinit_inline();
