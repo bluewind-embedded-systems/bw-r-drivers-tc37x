30a9f38ede0182234806fe5ea398f1e596ba4537
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-02-23 17:37:06 +0100

    Review msg_tx code

--- src/can/reg/msg_tx.rs
+++ src/can/reg/msg_tx.rs
@@ -1,12 +1,11 @@
 // TODO Remove this once the code is stable
 #![allow(clippy::undocumented_unsafe_blocks)]
 
-// TODO Is this module needed?
-// TODO Sus, this seems to be part of pac
+// Note: this module try to mimic the behavior of the pac module, for message SRAM access
+// Note: transmute is used to create a Reg from a pointer, because the pac module does not support creating Reg from pointers
 
+use crate::pac::common::{hidden::RegValue, Reg, RegisterField, RegisterFieldBool, RW};
 use core::mem::transmute;
-use tc37x_pac::common;
-use tc37x_pac::common::hidden::RegValue;
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub struct TxMsg(pub(super) *mut u8);
@@ -14,17 +13,17 @@ unsafe impl Send for TxMsg {}
 unsafe impl Sync for TxMsg {}
 impl TxMsg {
     #[inline(always)]
-    pub fn t0(self) -> common::Reg<T0, common::RW> {
+    pub fn t0(self) -> Reg<T0, RW> {
         let ptr = unsafe { self.0.add(0usize) };
         unsafe { transmute(ptr) }
     }
     #[inline(always)]
-    pub fn t1(self) -> common::Reg<T1, common::RW> {
+    pub fn t1(self) -> Reg<T1, RW> {
         let ptr = unsafe { self.0.add(4usize) };
         unsafe { transmute(ptr) }
     }
     #[inline(always)]
-    pub fn db(self) -> common::Reg<Db, common::RW> {
+    pub fn db(self) -> Reg<Db, RW> {
         let ptr = unsafe { self.0.add(8usize) };
         unsafe { transmute(ptr) }
     }
@@ -53,21 +52,21 @@ impl RegValue for T0 {
 }
 impl T0 {
     #[inline(always)]
-    pub fn id(self) -> common::RegisterField<0, 0x1FFFFFFF, 1, 0, u32, T0, common::RW> {
+    pub fn id(self) -> RegisterField<0, 0x1FFFFFFF, 1, 0, u32, T0, RW> {
         unsafe { transmute((self, 1)) }
     }
 
     #[inline(always)]
-    pub fn rtr(self) -> common::RegisterFieldBool<29, 1, 0, T0, common::RW> {
+    pub fn rtr(self) -> RegisterFieldBool<29, 1, 0, T0, RW> {
         unsafe { transmute((self, 1)) }
     }
 
     #[inline(always)]
-    pub fn xtd(self) -> common::RegisterFieldBool<30, 1, 0, T0, common::RW> {
+    pub fn xtd(self) -> RegisterFieldBool<30, 1, 0, T0, RW> {
         unsafe { transmute((self, 1)) }
     }
     #[inline(always)]
-    pub fn esi(self) -> common::RegisterFieldBool<31, 1, 0, T0, common::RW> {
+    pub fn esi(self) -> RegisterFieldBool<31, 1, 0, T0, RW> {
         unsafe { transmute((self, 1)) }
     }
 }
@@ -95,27 +94,27 @@ impl RegValue for T1 {
 }
 impl T1 {
     #[inline(always)]
-    pub fn dlc(self) -> common::RegisterField<16, 0xf, 1, 0, u8, T1, common::RW> {
+    pub fn dlc(self) -> RegisterField<16, 0xf, 1, 0, u8, T1, RW> {
         unsafe { transmute((self, 1)) }
     }
 
     #[inline(always)]
-    pub fn brs(self) -> common::RegisterFieldBool<20, 1, 0, T1, common::RW> {
+    pub fn brs(self) -> RegisterFieldBool<20, 1, 0, T1, RW> {
         unsafe { transmute((self, 1)) }
     }
 
     #[inline(always)]
-    pub fn fdf(self) -> common::RegisterFieldBool<21, 1, 0, T1, common::RW> {
+    pub fn fdf(self) -> RegisterFieldBool<21, 1, 0, T1, RW> {
         unsafe { transmute((self, 1)) }
     }
 
     #[inline(always)]
-    pub fn efc(self) -> common::RegisterFieldBool<23, 1, 0, T1, common::RW> {
+    pub fn efc(self) -> RegisterFieldBool<23, 1, 0, T1, RW> {
         unsafe { transmute((self, 1)) }
     }
 
     #[inline(always)]
-    pub fn mm(self) -> common::RegisterField<24, 0xff, 1, 0, u8, T1, common::RW> {
+    pub fn mm(self) -> RegisterField<24, 0xff, 1, 0, u8, T1, RW> {
         unsafe { transmute((self, 1)) }
     }
 }
