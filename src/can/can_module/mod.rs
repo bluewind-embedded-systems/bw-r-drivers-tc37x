4c4ace6c9fb5a8945a9fc4078afb78bf0e5d142b
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-03-04 18:31:49 +0100

    Fix for new pac

--- src/can/can_module/mod.rs
+++ src/can/can_module/mod.rs
@@ -91,9 +91,9 @@ macro_rules! impl_can_module {
                 // Enable CCCE and CI
                 let mcr = mcr
                     .ccce()
-                    .set(true)
+                    .set(1u8.into())
                     .ci()
-                    .set(true);
+                    .set(1u8.into());
 
                 // SAFETY: CCCE and CI are RW bits, bits 23:8 are written with 0
                 unsafe { $module_reg.mcr().write(mcr) }
@@ -126,10 +126,10 @@ macro_rules! impl_can_module {
                 let mcr = unsafe { $module_reg.mcr().read() };
 
                 let actual_clock_source = match clock_select.0 {
-                    0 => mcr.clksel0().get(),
-                    1 => mcr.clksel1().get(),
-                    2 => mcr.clksel2().get(),
-                    3 => mcr.clksel3().get(),
+                    0 => mcr.clksel0().get().0,
+                    1 => mcr.clksel1().get().0,
+                    2 => mcr.clksel2().get().0,
+                    3 => mcr.clksel3().get().0,
                     _ => unreachable!(),
                 };
 
