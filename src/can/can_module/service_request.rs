18beecfe752bb47106c84e7ad14da6fea79aaa02
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-03-04 16:25:25 +0100

    Fix for new pac

--- src/can/can_module/service_request.rs
+++ src/can/can_module/service_request.rs
@@ -1,15 +1,16 @@
 use crate::can::{InterruptLine, Module0, Module1, Tos};
 use crate::cpu::Priority;
+use tc37x::src::can::can_can::CaNxInTy_SPEC;
 use tc37x::{Reg, RW};
 
 // Note: for simplicity, this wraps a value of Can0Int0 type, even if the
 // underlying registers have different types in the PAC crate.
 // TODO This is technically correct, but it is bypassing PAC type-safety. We should discuss about this.
-pub(crate) struct ServiceRequest(Reg<u32, RW>);
+pub(crate) struct ServiceRequest(Reg<CaNxInTy_SPEC, RW>);
 
 impl Module0 {
     pub(crate) fn service_request(line: InterruptLine) -> ServiceRequest {
-        let line_index = usize::from(line);
+        let line_index = usize::from(line as u8);
         let x = tc37x::SRC.can().can_can()[0].canxinty()[line_index];
         ServiceRequest(x)
     }
@@ -17,7 +18,7 @@ impl Module0 {
 
 impl Module1 {
     pub(crate) fn service_request(line: InterruptLine) -> ServiceRequest {
-        let line_index = usize::from(line);
+        let line_index = usize::from(line as u8);
         let x = tc37x::SRC.can().can_can()[1].canxinty()[line_index];
         ServiceRequest(x)
         // ServiceRequest(match line {
@@ -34,14 +35,15 @@ impl ServiceRequest {
 
         // Set priority and type of service
         // SAFETY: FIXME Check Aurix manual, tos is in range [0, 3], bits 9:8, 15:14, 23:21, 31 are written with 0
-        unsafe { self.0.modify(|r| r.srpn().set(priority).tos().set(tos)) };
+        // TODO .tos() è finito in un altro registro, cerca in can0
+        // unsafe { self.0.modify(|r| r.srpn().set(priority).tos().set(tos)) };
 
         // Clear request
         // SAFETY: CLRR is a W bit, bits 9:8, 15:14, 23:21, 31 are written with 0
-        unsafe { self.0.modify(|r| r.clrr().set(true)) };
+        unsafe { self.0.modify(|r| r.clrr().set(1u8.into())) };
 
         // Enable service request
         // SAFETY: SRE is a RW bit, bits 9:8, 15:14, 23:21, 31 are written with 0
-        unsafe { self.0.modify(|r| r.sre().set(true)) };
+        unsafe { self.0.modify(|r| r.sre().set(1u8.into())) };
     }
 }
