90fc07d796951dab1f6242bc71e812afdd85ae42
federico.pezzato <federico.pezzato@bluewind.it>
2024-03-13 17:55:07 +0100

    [WIP] extend can tests

--- tests/can.rs
+++ tests/can.rs
@@ -1,9 +1,8 @@
 use tc37x_driver::tracing::log::Report;
-use tc37x_driver::can::Module;
-use tc37x_driver::can::Module0;
+use tc37x_driver::can::{Module, Module0, NodeConfig, BitTimingConfig, AutoBitTiming, Node0};
 
 #[test]
-fn test_can_module_enable(){
+fn test_can_module_take_node(){
     let report = Report::new();
     let can_module = Module::new(Module0);
 
@@ -29,9 +28,33 @@ fn test_can_module_enable(){
     report.expect_read(0xF003624C, 4, 0b1111111111111100_00000000111100_0_1);
     report.expect_read(0xF003624C, 4, 0b1111111111111100_00000000111100_0_1);
 
-
-
     let mut can_module = can_module.enable();
+
+    let cfg = NodeConfig {
+        baud_rate: BitTimingConfig::Auto(AutoBitTiming {
+            baud_rate: 1_000_000,
+            sample_point: 8_000,
+            sync_jump_width: 3,
+        }),
+        ..Default::default()
+    };
+
+    // mcr read for set clock source
+    // report.expect_read(0xF0208030, 4, 0x0000);
+    // report.expect_read(0xF0208030, 4, 0b0_0_0_0_0_000_0000000000000000_00_00_00_11);
+
+    // cccr read for enable configuration change
+    // FIXME it's not working
+    // report.expect_read(0xF0208218, 4, 0b000000000000000000000000000000_0_1);
+    // report.expect_read(0xF0208218, 4, 0b000000000000000000000000000000_0_1);
+    // report.expect_read(0xF0208218, 4, 0b000000000000000000000000000000_0_0);
+    // report.expect_read(0xF0208218, 4, 0b000000000000000000000000000000_0_1);
+
+    // ccucon1 read for get_mcan_frequency for configure_baud_rate
+    // ccucon0 read for get_source_frequency for get_mcan_frequency
+
+
+    // let mut node = can_module.take_node(Node0, cfg).expect("Cannot take can node");
 
     insta::assert_snapshot!(report.take_log());
 }
